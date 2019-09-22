3b5501a430be900888f4beb38c9f99f08dec13be
Ailan <alcantara2575@gmail.com>
2019-09-22 16:16:47 -0400

    Pagina funcionando

---
+++ README.md
@@ -0,0 +1,2 @@
+# data_show
+# Este es un ejemplo del modo de mostrar la informacion a los usuarios
--- start.py
+++ start.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def saluda():
     return render_template("index.html")
 
+@app.route('/<string:page>')
+def pages(page):
+    return render_template( page )
+    
 if __name__ == '__main__':
     app.run(debug=True)
